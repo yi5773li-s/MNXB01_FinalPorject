98529e2577597b07f90361f631679d735cb5bf5b
Martin Waites <nat15mwa@student.lu.se>
2019-11-06 17:11:30 +0100

    Redone LattDiff scrap idea of calling everyDay as i could not get it to work finaly works in a basic way

--- project/project.C
+++ project/project.C
@@ -8,38 +8,34 @@
 #include <vector>
 #include <TGraphErrors.h>
 
-Double_t * oneDay(string filePath, Int_t day, Int_t plot=1){
+Int_t n=365;
+Double_t outputMS[2];
+
+Double_t * oneDay(string filePath, Int_t day, Int_t plot=0){
 	
-	
-	
-	
-	
-	static Double_t result[2];
 	if (filePath == "test0"){
 		Double_t tday = (Double_t)day;
-		result[0]=tday+2.2;
-		result[1]=tday/3;
-		return result;
+		outputMS[0]=tday+2.2;
+		outputMS[1]=tday/3;
+		return outputMS;
 	}
-	if (filePath == "test1"){
+	else if (filePath == "test1"){
 		Double_t tday = (Double_t)day;
-		result[0]=-tday-2.2;
-		result[1]=tday/3;
-		return result;
+		outputMS[0]=-tday+8.2;
+		outputMS[1]=tday/3;
+		return outputMS;
 	}
-	if (filePath == "test2"){
+	else if (filePath == "test2"){
 		Double_t tday = (Double_t)day;
-		result[0]=tday+4.2;
-		result[1]=tday/2;
-		return result;
+		outputMS[0]=tday+4.2;
+		outputMS[1]=tday/2;
+		return outputMS;
 	}
 }
 
-Double_t * everyDay(string filePath="../clean_data/upsala_clean.dat", Int_t plot=1){
-	Int_t n=365;
+Double_t * everyDay(string filePath="../clean_data/upsala_clean.dat", Int_t plot=0){
 	Double_t *p;
 	Double_t daysMean[n],daysStaDev[n],days[n],zeros[n];
-	static Double_t store[365];
 	//vector<Double_t> daysMean;
 	//vector<Double_t> daysStaDev;
 	//vector<Int_t> days;
@@ -54,15 +50,16 @@ Double_t * everyDay(string filePath="../clean_data/upsala_clean.dat", Int_t plot
 		//daysStaDev.push_back(p[1]);
 		//days.push_back(i);
 		//zeros.push_back(0);
-		if (plot == 0){
-			store[i] = daysMean[i];
-			
+		/*
+		if (plot == 1){
+			outputBigM[i] = daysMean[i];
 		}
 		if (plot == 2){
-			store[i] = daysStaDev[i];
+			outputBigS[i] = daysStaDev[i];
 		}
+		*/
 	}
-	if (plot == 1){
+	if (plot == 0){
 		gStyle->SetOptStat(0);
 		gStyle->SetOptTitle(0);
 		gStyle->SetTitleSize(0.05, "x");
@@ -78,64 +75,81 @@ Double_t * everyDay(string filePath="../clean_data/upsala_clean.dat", Int_t plot
 		g1->Draw("AL");
 		c1->SaveAs("every_day_graph_one_location.jpg");
 	}
+	/*
 	if (plot == 0){
-		return store;
+		return outputBigM;
 	}	
+	if (plot == 2){
+		return outputBigS;
+	}
+	*/
 }
 
 
 void LattDiff(){
-	Int_t n=365;
 	Double_t days[365];
 	Double_t zeros[365];
 	/*
-	Double_t *lund, *visby, *upsala, *umea, *luleu;
-	lund = everyDay("../clean_data/lund_clean.dat",0);
-	visby = everyDay("../clean_data/visby_clean.dat",0);
-	upsala = everyDay("../clean_data/upsala_clean.dat",0);
-	umea = everyDay("../clean_data/umea_clean.dat",0);
-	luleu = everyDay("../clean_data/luleu_clean.dat",0);
-	Double_t lundM[n],lundS[n];
-	Double_t visbyM[n],visbyS[n];
-	Double_t upsalaM[n],upsalaS[n];
-	Double_t umeaM[n],umeaS[n];
-	Double_t luleuM[n],luleuS[n];
+	Double_t *lundM, *visbyM, *upsalaM, *umeaM, *luleuM;
+	lundM = everyDay("../clean_data/lund_clean.dat",0);
+	visbyM = everyDay("../clean_data/visby_clean.dat",0);
+	upsalaM = everyDay("../clean_data/upsala_clean.dat",0);
+	umeaM = everyDay("../clean_data/umea_clean.dat",0);
+	luleuM = everyDay("../clean_data/luleu_clean.dat",0);
+	
+	Double_t *lundS, *visbyS, *upsalaS, *umeaS, *luleuS;
+	lundS = everyDay("../clean_data/lund_clean.dat",0);
+	visbyS = everyDay("../clean_data/visby_clean.dat",0);
+	upsalaS = everyDay("../clean_data/upsala_clean.dat",0);
+	umeaS = everyDay("../clean_data/umea_clean.dat",0);
+	luleuS = everyDay("../clean_data/luleu_clean.dat",0);
+	
+	Double_t *test0M, *test0S, *test1M, *test1S, *test2M, *test2S;
+	test0M = everyDay("test0",1);
+	test0S = everyDay("test0",2);
+	test1M = everyDay("test1",1);
+	test1S = everyDay("test1",2);
+	test2M = everyDay("test2",1);
+	test2S = everyDay("test2",2);
 	*/
-	Double_t *test01, *test02, *test1, *test2;
-	test01 = everyDay("test0",0);
-	test02 = everyDay("test0",2);
-	test1 = everyDay("test1",0);
-	test2 = everyDay("test2",0);
-	Double_t test0M[365],test0S[365],test1M[n],test1S[n],test2M[n],test2S[n];
-	for (Int_t i=0; i < 364; i++){
+	Double_t *t0, *t1, *t2;
+	Double_t t0daysMean[n],t0daysStaDev[n],t1daysMean[n],t1daysStaDev[n];
+	for(Int_t i = 0; i < n; i++){
 		days[i]=i+1;
-		zeros[i]=0;		
-		
-		test0M[i] = test01[i];
-		test0S[i] = test02[i];
-		/*
-		test1M[i] = test1[0][i];
-		test1S[i] = test1[1][i];
-		test2M[i] = test2[0][i];
-		test2S[i] = test2[1][i];
-		
-		lundM[i] = lund[0][i];
-		lundS[i] = lund[1][i];
-		visbyM[i] = visby[0][i];
-		visbyS[i] = visby[1][i];
-		upsalaM[i] = upsala[0][i];
-		upsalaS[i] = upsala[1][i];
-		umeaM[i] = umea[0][i];
-		umeaS[i] = umea[1][i];
-		luleuM[i] = luleu[0][i];
-		luleuS[i] = luleu[1][i];
-		*/
+		zeros[i]=0;
+		t0 = oneDay("test0",i,0);
+		t0daysMean[i]=t0[0];
+		t0daysStaDev[i]=t0[1];
+		t1 = oneDay("test1",i,0);
+		t1daysMean[i]=t1[0];
+		t1daysStaDev[i]=t1[1];
 	}
-	//TCanvas* c2 = new TCanvas("c2", "every day graph multiple locations", 900, 600);
-	TGraphErrors* t0 = new TGraphErrors(n,days,test0M,zeros,test0S);
-	t0->Draw("AL");
-	//TGraphErrors* t1 = new TGraphErrors(n,days,test1M,zeros,test1S);
-	//t1->Draw("L");
+	
+	
+	
+	
+	for (Int_t i=0; i < 365; i++){
+		cout << t0daysMean[i] << endl;
+		cout << t1daysMean[i] << endl;
+		
+	}
+	TCanvas* c2 = new TCanvas("c2", "every day graph multiple locations", 900, 600);
+	gStyle->SetOptStat(0);
+	gStyle->SetOptTitle(0);
+	gStyle->SetTitleSize(0.05, "x");
+	gStyle->SetTitleSize(0.05, "y");
+	gStyle->SetLabelSize(0.05, "x");
+	gStyle->SetLabelSize(0.05, "y");
+	gStyle->SetPadTopMargin(0.05);
+	gStyle->SetPadRightMargin(0.05);
+	gStyle->SetPadBottomMargin(0.16);
+	gStyle->SetPadLeftMargin(0.16);
+	TGraphErrors* t0p = new TGraphErrors(n,days,t0daysMean,zeros,t0daysStaDev);
+	t0p->SetMaximum(400);
+	t0p->SetMinimum(-400);
+	t0p->Draw("AL");
+	TGraphErrors* t1p = new TGraphErrors(n,days,t1daysMean,zeros,t1daysStaDev);
+	t1p->Draw("L");
 	//TGraphErrors* t2 = new TGraphErrors(n,days,test2M,zeros,test2S);
 	//t2->Draw("L");
 	
@@ -143,6 +157,7 @@ void LattDiff(){
 	
 	
 	
-	//c2->SaveAs("every_day_graph_multiple_locations.jpg");
+	c2->SaveAs("every_day_graph_multiple_locations.jpg");
 
 }
+
