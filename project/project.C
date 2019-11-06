722383833e66f64602c791d8b259364228b60bfb
Martin Waites <nat15mwa@student.lu.se>
2019-11-06 13:39:29 +0100

    added stuff to first exercise to be able to test second, continued work on second now that i could test it

--- project/project.C
+++ project/project.C
@@ -7,20 +7,34 @@
 #include <TCanvas.h>
 #include <vector>
 
-Double_t oneDay(string filePath, Int_t day, Int_t plot=1){
-
-
-
-
+Double_t * oneDay(string filePath, Int_t day, Int_t plot=1){
+	
+	
+	
+	
+	
+	static Double_t result[2];
+	if (filePath == "test"){
+		Double_t tday = (Double_t)day;
+		result[0]=tday+2.2;
+		result[1]=tday/3;
+		return result;
+	}
 }
 
 void everyDay(string filePath){
-  vector<Double_t> daysMean;
-  vector<Double_t> daysStaDev;
-  for(Int_t i = 1, i < 365, i++){
-    daysMean.push_back(oneDay(filePath,i,0)[0])
-    daysStaDev.push_back(oneDay(filePath,i,0)[1])
-  }
+	Double_t *p;
+	vector<Double_t> daysMean;
+	vector<Double_t> daysStaDev;
+	vector<Int_t> days;
+	for(Int_t i = 1; i <= 365; i++){
+		p = oneDay(filePath,i,0);
+		daysMean.push_back(p[0]);
+		daysStaDev.push_back(p[1]);
+		days.push_back(i);
+	}
+	
+	
 
 
 
